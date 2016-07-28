0f0e624c067d36dc2989df66cca645277029bc76
marcurious <radovic.b.marko@gmail.com>
2016-07-28 09:04:23 +0200

    Adding initial controller

---
+++ src2/ComicBookGallery2/Controllers/ComicBooksGalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ComicBookGallery2.Controllers
+{
+    public class ComicBooksGalleryController : Controller
+    {
+        public string Detail()
+        {
+            return "Hello!";
+        }
+    }
+}
