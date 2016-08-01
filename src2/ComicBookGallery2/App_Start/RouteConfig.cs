b36f7224967cc6ed5dbe7705674b8f125b8a2be0
marcurious <radovic.b.marko@gmail.com>
2016-08-01 23:18:14 +0200

    Added get back to list button

--- src2/ComicBookGallery2/App_Start/RouteConfig.cs
+++ src2/ComicBookGallery2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ComicBookGallery2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "ComicBooksGallery", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
